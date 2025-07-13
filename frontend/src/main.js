bf024f238340e5962eef72d74dabf1ac9f36b4ce
Aditya Savara <39929571+AdityaSavara@users.noreply.github.com>
2025-07-13 19:41:36 -0400

    upper graph download mostly working

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,10 @@ import './app.css';
 
 import {GetJSONFromFile, CreateExampleJSON} from '../wailsjs/go/main/App';
 import { mergeAndplotData, clearData } from './JSONGrapher/src/index.js';
-
+import { createCSV } from './JSONGrapher/src/fileUtils.js';
+import {addOpeningURLonButtonClick} from './JSONGrapher/src/linkUtils.js';
 document.querySelector('#app').innerHTML = ``;
+
 
 //////Start of block of code for upper JSONGrapher example which changes graphs with button click. 
 
@@ -22,9 +24,11 @@ upperGraphOuterDiv.innerHTML = `
     <h2><font color='black'>This is a simple JSONGrapher Wails App to Demonstrate Showing Graphs from JSON</font></h2>
     <div id="graphDivGraph2"></div>
     <div id="errorMessagesDivGraph2"></div>
-    Click on 'Change Graph' to switch between graphs.
+  Click on 'Change Graph' to switch between graphs.<br>
 	<button class="btn" onclick="changeGraph()">Change Graph</button> 
-	<button class="btn" onclick="clearUpperGraph()">Clear Upper Graph</button> 
+	<button class="btn" onclick="clearUpperGraph()">Clear Upper Graph</button> <br>
+  <button class="btn" id="downloadUpperGraphJSON-btn" onclick="downloadUpperGraphJSON()">Download JSON</button> 
+  <button class="btn" id="downloadUpperGraphCSV-btn" onclick="downloadUpperGraphCSV()">Download CSV</button>   
   </center>
 `;
 //I did consider using this as well:   <button class="btn" onclick="clearData(graphDivGraph2)">Clear Graph</button>
@@ -40,43 +44,66 @@ const messagesToUserDiv = "This is a message for the user upon loading.";
 
 //let's plot a graph. The path used will be relative to main.go. The string below is the graph filename and does not load it again.
 let mergedFigDict = await mergeAndplotData(null, providedJson1, "SrTiO3_rainbow.json", graphDivGraph2, messagesToUserDiv, errorMessagesDivGraph2);
-let currentGraphDataSource = "SrTiO3_rainbow.json";
+let currentUpperGraphDataSource = "SrTiO3_rainbow.json";
+let currentUpperGraphJson 
 
 //We'll make a little function to switch between graphs, then populate the window with it.
 //This function will utilize this module's 'currentGraphDataSource' variable to know which graph is currently up.
+//NOTE: for the download links, we are using variables that are global across this file.
 async function changeGraph(){
 	//This function checks which of two prechosen graphs is loaded, then clears and loads the other graph.
-  if (currentGraphDataSource === "SrTiO3_rainbow.json"){
+  if (currentUpperGraphDataSource === "SrTiO3_rainbow.json"){
     //change to the other graph, start with clearing.
-	//clearing a graph just requires passing in its Div and optionally the document it is in.
-	await clearData(graphDivGraph2, document);
+    //clearing a graph just requires passing in its Div and optionally the document it is in.
+    await clearData(graphDivGraph2, document);
     //now load the other graph. Though we don't need to read file in again... we will, since we are 
     // trying to create behavior of when a new json will come from Go, and this does that.
     providedJson2 = await GetJSONFromFile("./json_records/DRIFTS_CO_Adsorption_onAu22_offset2d.json");
-    mergedFigDict = await mergeAndplotData(null, providedJson2, "DRIFTS_CO_Adsorption_onAu22_offset2d.json", graphDivGraph2, messagesToUserDiv, errorMessagesDivGraph2);
-    currentGraphDataSource = "DRIFTS_CO_Adsorption_onAu22_offset2d.json";     //update the currentGraphDataSource variable, which is just a name.
-  } else if (currentGraphDataSource === "DRIFTS_CO_Adsorption_onAu22_offset2d.json"){
+    currentUpperGraphJson = providedJson2
+    mergedFigDict = await mergeAndplotData(null, currentUpperGraphJson, "DRIFTS_CO_Adsorption_onAu22_offset2d.json", graphDivGraph2, messagesToUserDiv, errorMessagesDivGraph2);
+    currentUpperGraphDataSource = "DRIFTS_CO_Adsorption_onAu22_offset2d.json";     //update the currentGraphDataSource variable, which is just a name.
+  } else if (currentUpperGraphDataSource === "DRIFTS_CO_Adsorption_onAu22_offset2d.json"){
     //change to the other graph, start with clearing.
     //clearing a graph just requires passing in its Div and optionally the document it is in.
-	await clearData(graphDivGraph2, document);
-	//now load the other graph. Though we don't need to read file in again... we will, since we are 
+	  await clearData(graphDivGraph2, document);
+	  //now load the other graph. Though we don't need to read file in again... we will, since we are 
     // trying to create behavior of when a new json will come from Go, and this does that.
     providedJson1 = await GetJSONFromFile("./json_records/SrTiO3_rainbow.json");
-    mergedFigDict = await mergeAndplotData(null, providedJson1, "SrTiO3_rainbow.json", graphDivGraph2, messagesToUserDiv, errorMessagesDivGraph2);
-    currentGraphDataSource = "SrTiO3_rainbow.json";     //update the currentGraphDataSource variable
+    currentUpperGraphJson = providedJson1
+    mergedFigDict = await mergeAndplotData(null, currentUpperGraphJson, "SrTiO3_rainbow.json", graphDivGraph2, messagesToUserDiv, errorMessagesDivGraph2);
+    currentUpperGraphDataSource = "SrTiO3_rainbow.json";     //update the currentGraphDataSource variable
   }
 
 }
 
-
 async function clearUpperGraph(){
 	//clearing a graph just requires passing in its Div and optionally the document it is in.
 	await clearData(graphDivGraph2, document);
-
 };
-//Add the new function to the window so that the changeGraph button can access it.
+
+
+async function downloadUpperGraphJSON(){
+  let downloadButton = document.getElementById("downloadUpperGraphJSON-btn");    
+  const filename ="upperGraph.json"
+  const downloadLink = createDownloadJSONLink(currentUpperGraphJson, filename);
+  downloadButton = addOpeningURLonButtonClick (downloadButton, downloadLink)
+}
+
+async function downloadUpperGraphCSV(){
+  let downloadButton = document.getElementById("downloadUpperGraphCSV-btn");
+  //The csv String is in csvContent.csv
+  const csvContent = createCSV(currentUpperGraphJson);
+  const filename = "upperGraph.csv"
+  const downloadLink = createDownloadCSVLink(csvContent.csv, filename);  
+  downloadButton = addOpeningURLonButtonClick (downloadButton, downloadLink)
+}
+
+
+//Add the new functions to the window so that the buttons can access them.
 window.changeGraph = changeGraph;
 window.clearUpperGraph = clearUpperGraph;
+window.downloadUpperGraphCSV = downloadUpperGraphCSV;
+window.downloadUpperGraphJSON = downloadUpperGraphJSON;
 
 
 //////Start of block of code for upper JSONGrapher example which changes graphs with button click. 
@@ -93,16 +120,18 @@ lowerGraphOuterDiv.innerHTML = `
     <h2><font color='black'>Click on the various buttons to proceed with the demonstration.<br> You can also zoom by mouse and hover over individual points.</font></h2>
     <div id="graphDivGraph3"></div>
     <div id="errorMessagesDivGraph3"></div>
-    Click on 'Create Lower Graph' to create an additional graph, and 'Clear Lower Graph' to remove it. 
+  Click on 'Create Lower Graph' to create an additional graph, and 'Clear Lower Graph' to remove it. <br>
 	<button class="btn" onclick="createLowerGraph()">Create Lower Graph</button> 
-	<button class="btn" onclick="clearLowerGraph()">Clear Lower Graph</button> 
+	<button class="btn" onclick="clearLowerGraph()">Clear Lower Graph</button> <br>
+  <button class="btn" id="downloadLowerGraphJSON-btn" onclick="downloadLowerGraphJSON()">Download JSON</button> 
+  <button class="btn" id="downloadLowerGraphCSV-btn" onclick="downloadLowerGraphCSV()">Download CSV</button>   
   </center>
 `;
 
 
 // Append to desired location in the DOM
 document.querySelector('#app').appendChild(lowerGraphOuterDiv);
-
+let currentLowerGraphJson
 
 //This time, instead of making variables directly in main.js, we'll make them inside the createLowerGraph function.
 async function createLowerGraph(){
@@ -113,9 +142,9 @@ async function createLowerGraph(){
 	const errorMessagesDivGraph3 = document.getElementById("errorMessagesDivGraph3");
 	const graphDivGraph3 = document.getElementById("graphDivGraph3");
 	const messagesToUserDiv = "Click to create the lower graph or to clear it. Clicking to create multiple times will load the data multiple times.";
-
+  currentLowerGraphJson = graph3JSON
 	//let's plot a graph. We'll provide null for the filename.
-	let mergedFigDict = await mergeAndplotData(null, graph3JSON, null, graphDivGraph3, messagesToUserDiv, errorMessagesDivGraph3);	
+	let mergedFigDict = await mergeAndplotData(null, currentLowerGraphJson, null, graphDivGraph3, messagesToUserDiv, errorMessagesDivGraph3);	
 };
 
 async function clearLowerGraph(){
